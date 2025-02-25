cbd3ebc4f4d05f26be96b51805b057c45d3c826d
Claire310 <claireyang310@gmail.com>
2025-02-25 15:53:25 +0000

    新增分支，修改主程式

--- installapache.sh
+++ installapache.sh
@@ -4,4 +4,4 @@ wget https://www.free-css.com/assets/files/free-css-templates/download/page296/b
 unzip browny.zip &> /dev/null 
 sudo mv browny-v1.0/* /var/www/html/ 
 # echo "You can visit my web site now."
-dialog --title "完成安裝" --msgbox "\n恭喜你，可以開始瀏覽我的網站了~~" 10 37
+# dialog --title "完成安裝" --msgbox "\n恭喜你，可以開始瀏覽我的網站了~~" 10 37
