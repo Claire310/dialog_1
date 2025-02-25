42bff318993b5f21e8e2d2575eceae7b92a3b728
user <u36329531@gmail.com>
2025-02-25 21:48:46 +0800

    留存更改部分

--- installapache_dog.sh
+++
@@ -1,7 +0,0 @@
-sudo apt install apache2 -y &> /dev/null 
-# echo "Apache2 installed." 
-wget https://www.free-css.com/assets/files/free-css-templates/download/page291/drool.zip &> /dev/null
-unzip drool.zip &> /dev/null 
-sudo mv drool-html/* /var/www/html/ 
-# echo "You can visit my web site now."
-dialog --title "完成安裝" --msgbox "\n恭喜你，可以開始瀏覽我的網站了~~" 10 37
